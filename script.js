b2fe2db6e57a6de93d3eb442e1a09e7528bb988e
Kristopher <kristopher_matic@hotmail.com>
2022-07-26 12:37:19 -0700

    finish project design changes fully functional

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ function Book(title, author, pages, status) {
     this.author = author;
     this.pages = pages;
     this.status = status;
+    this.toggleStatus = function() {
+        if (this.status === 'NOT READ') {
+            this.status = 'READ';
+        } else {
+            this.status = 'NOT READ';
+        }
+    }
 }
 
 function addBookToLibrary() {
@@ -15,31 +22,49 @@ function addBookToLibrary() {
     const myAuthor = document.querySelector('#author').value;
     const myPages = document.querySelector('#pages').value;
     const readStatus = document.querySelector('#read').value;
-
     const newBook = new Book(myTitle, myAuthor, myPages, readStatus);
     myLibrary.push(newBook);
     displayBook();
 }
 
 function displayBook() {
+    cardContainer.innerHTML = '';
     for (let i = 0; i < myLibrary.length; i++) {
         const newCard = document.createElement('div');
-        const newTitle = document.createElement('p');
+        const newTitle = document.createElement('h2');
         const newAuthor = document.createElement('p');
         const newPages = document.createElement('p');
-        const newRead = document.createElement('p');
+        const readBtn = document.createElement('button');
+        const delBtn = document.createElement('button');
 
         newTitle.textContent = myLibrary[i].title;
-        newAuthor.textContent = myLibrary[i].author;
-        newPages.textContent = myLibrary[i].pages;
-        newRead.textContent = myLibrary[i].status;
+        newAuthor.textContent = `Author: ${myLibrary[i].author}`;
+        newPages.textContent = `Pages: ${myLibrary[i].pages}`;
+        readBtn.textContent = myLibrary[i].status;
+        delBtn.textContent = 'DELETE';
+        delBtn.dataset.indexNumber = i;
 
         newCard.classList.add('book-card');
+        readBtn.classList.add('read-btn');
+        delBtn.classList.add('del-btn');
+
         newCard.appendChild(newTitle);    
         newCard.appendChild(newAuthor);    
         newCard.appendChild(newPages);    
-        newCard.appendChild(newRead);
-        cardContainer.appendChild(newCard);    
+        newCard.appendChild(readBtn);
+        newCard.appendChild(delBtn);
+        cardContainer.appendChild(newCard);
+
+        delBtn.addEventListener('click', () => {
+            let index = delBtn.dataset.indexNumber;
+            myLibrary.splice(index, 1);
+            displayBook();
+        });
+
+        readBtn.addEventListener('click', () => {
+            myLibrary[i].toggleStatus();
+            displayBook();
+        })
     }
 }
 
