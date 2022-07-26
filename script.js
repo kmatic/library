956df37c7fc2a5ee4e255fadb6e0e72c823a9c4a
Kristopher <kristopher_matic@hotmail.com>
2022-07-26 10:35:06 -0700

    linked js and html DOM to add books

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let myLibrary = [];
 
+const addBtn = document.querySelector('.btn');
+const cardContainer = document.querySelector('.main')
+
 function Book(title, author, pages, status) {
     this.title = title;
     this.author = author;
@@ -8,18 +11,36 @@ function Book(title, author, pages, status) {
 }
 
 function addBookToLibrary() {
-    let title = prompt('title');
-    let author = prompt('author');
-    let pages = prompt('pages');
-    let status = prompt ('read');
+    const myTitle = document.querySelector('#title').value;
+    const myAuthor = document.querySelector('#author').value;
+    const myPages = document.querySelector('#pages').value;
+    const readStatus = document.querySelector('#read').value;
 
-    const newBook = new Book(title, author, pages, status);
+    const newBook = new Book(myTitle, myAuthor, myPages, readStatus);
     myLibrary.push(newBook);
     displayBook();
 }
 
 function displayBook() {
     for (let i = 0; i < myLibrary.length; i++) {
-        console.log(myLibrary);
+        const newCard = document.createElement('div');
+        const newTitle = document.createElement('p');
+        const newAuthor = document.createElement('p');
+        const newPages = document.createElement('p');
+        const newRead = document.createElement('p');
+
+        newTitle.textContent = myLibrary[i].title;
+        newAuthor.textContent = myLibrary[i].author;
+        newPages.textContent = myLibrary[i].pages;
+        newRead.textContent = myLibrary[i].status;
+
+        newCard.classList.add('book-card');
+        newCard.appendChild(newTitle);    
+        newCard.appendChild(newAuthor);    
+        newCard.appendChild(newPages);    
+        newCard.appendChild(newRead);
+        cardContainer.appendChild(newCard);    
     }
 }
+
+addBtn.addEventListener('click', () => addBookToLibrary());
